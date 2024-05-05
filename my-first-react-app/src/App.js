d2b2e3f1865683fe9a471fdf49262878512a20df
KAMRAN\kamranali <kamranali.says@gmail.com>
2024-05-05 23:57:08 +0100

    Adding current time variable.

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-   <h1> Hello World ..!</h1>
+   const currentDate = new Date();
+
+   return (
+     <div> 
+           <h1> Hello World ..!</h1>
+           <h2> the current time {currentDate.toLocaleTimeString()}</h2>
+    </div>
+
   );
 }
 
